9ca5bf2187ae091a40b21e478c0c914ef8f6773d
John-Alz <99442406+John-Alz@users.noreply.github.com>
2025-02-28 06:47:55 -0500

    feat: setup

---
+++ src/RecipePageApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const RecipePageApp = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-Outfit'>RecipePageApp</h1>
+        </div>
+    )
+}
