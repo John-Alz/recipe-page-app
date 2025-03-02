e0a7f0acee72da7713b79ef0cb1a696a81f801ee
John-Alz <99442406+John-Alz@users.noreply.github.com>
2025-03-01 19:18:19 -0500

    feat: detail page ingredients

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -4,7 +4,6 @@ import { useContext } from 'react'
 import { RecipeContext } from '../context/RecipeContext'
 import { Link, useParams } from 'react-router-dom'
 
-let ingredients = {};
 
 export const RecipeDetail = () => {
 
@@ -16,19 +15,24 @@ export const RecipeDetail = () => {
 
     useFetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`)
 
+    let ingredients = [];
 
+    for (let i = 0; i < mealDetail.length; i++) {
+        for (let j = 1; j <= 20; j++) {
+            let ingredientKey = `strIngredient${j}`;
+            let measureKey = `strMeasure${j}`;
 
+            let name = mealDetail[i][ingredientKey]?.trim();
+            let cant = mealDetail[i][measureKey]?.trim();
 
-
-    for (let i = 0; i <= mealDetail.length; i++) {
-
-        console.log(mealDetail[i]?.strIngredient);
+            if (name) {
+                ingredients.push({
+                    name: name,
+                    cant: cant || ""
+                });
+            }
+        }
     }
-
-    console.log(ingredients);
-
-
-
 
     return (
         <div>
@@ -44,6 +48,27 @@ export const RecipeDetail = () => {
                 <div className='flex gap-3 pt-8'>
                     <p className='bg-card py-2 px-4 rounded-full'>category: <span className='font-bold'>{mealDetail[0]?.strCategory}</span></p>
                     <p className='bg-card py-2 px-4 rounded-full'>area: <span className='font-bold'>{mealDetail[0]?.strArea}</span></p>
+                </div>
+                <div>
+                    <div className='flex items-center gap-2 mt-6 mb-4'>
+                        <div className='bg-hover w-[20px] h-[40px] rounded-2xl'></div>
+                        <h2 className='font-bold'>Ingredients</h2>
+                    </div>
+                    <ul className='list-disc ml-5'>
+                        {
+                            ingredients.map((ingredient) => (
+                                <li>{ingredient.cant} {ingredient.name}</li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <div className='flex items-center gap-2 mt-6 mb-4'>
+                        <div className='bg-azul w-[20px] h-[40px] rounded-2xl'></div>
+                        <h2 className='font-bold'>Instructions</h2>
+
+                    </div>
+                    <p>{mealDetail[0]?.strInstructions}</p>
                 </div>
             </section>
         </div>
